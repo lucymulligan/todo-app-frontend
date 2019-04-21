691c5b66306e94fa48b8dfd62e919dea038f11e9
lucymulligan <lucymulligan@hotmail.com>
2019-04-21 08:24:25 +0100

    Added in font from Google Fonts

--- src/App.js
+++ src/App.js
@@ -4,22 +4,29 @@ import Nav from './components/Nav';
 import AddItem from './components/AddItem';
 import ListItem from './components/ListItem';
 
+const toDoListItems = [
+  "Garden the plants",
+  "Make an omlette",
+  "Bath the cat"
+]
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Nav text="Welcome to my app"/>
+      <Nav text="Welcome to your toDo application!"/>
       <Nav text="Read more about it"/>
       <Nav text="Partners"/>
       <Nav text="About us"/>
       <AddItem />
         <h4>Here's just a list of things that need to get done.</h4>
         <ul>
-          <ListItem task ="Walk the Dog" />
-          <ListItem task ="Water the plants :)" />
-          <ListItem task ="Get an Easter Egg" />
+          {toDoListItems.map(function (item) {
+            return <ListItem task={item} />;
+          })}
         </ul>
       </div>
+    
     );
   }
 }
